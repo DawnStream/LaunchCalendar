dc7d334b9d1980f7858450c26950da76207d98bd
Arik <arik.lital@gmail.com>
2015-05-05 10:06:49 -0500

    update express version, trying to update rocket route

--- routes/rocketRoute.js
+++ routes/rocketRoute.js
@@ -1,30 +1,33 @@
 /**
  * Created by ariky on 4/3/15.
  */
-rocketController = require('../controllers/RocketController');
+var express = require('express');
+var rocketController = require('../controllers/RocketController');
+var router = express.Router();
+var controller = rocketController.getObject();
 
-module.exports =function(app) {
-    app.get('/rocket/manufacturer/:manufacturer', function(req, res) {
-        rocketController.getManufacturer(req.params.manufacturer, function (err, rockets) {
-            if (err) {
-                res.send(400, err);
-            }
-            else {
-                console.log(rockets);
-                res.json(rockets);
-            }
-        })
-    });
+router.get('/manufacturer/:manufacturer', function(req, res) {
+    controller.getManufacturer(req.params.manufacturer, function (err, rockets) {
+        if (err) {
+            res.send(400, err);
+        }
+        else {
+            console.log(rockets);
+            res.json(rockets);
+        }
+    })
+});
 
-    app.get('/rocket/model/:model', function(req, res) {
-        rocketController.getModel(req.params.model, function (err, rockets) {
-            if (err) {
-                res.send(400, err);
-            }
-            else {
-                console.log(rockets);
-                res.json(rockets);
-            }
-        })
-    });
-}
+router.get('/model/:model', function(req, res) {
+    controller.getModel(req.params.model, function (err, rockets) {
+        if (err) {
+            res.send(400, err);
+        }
+        else {
+            console.log(rockets);
+            res.json(rockets);
+        }
+    })
+});
+
+module.exports = router;
